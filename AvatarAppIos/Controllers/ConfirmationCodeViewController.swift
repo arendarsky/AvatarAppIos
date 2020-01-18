b32dfaf5f04ec9bad8152572a396a83211ea1485
pycak0 <vladisrusakov@yandex.ru>
2020-01-18 15:59:15 +0300

    Performed hiding the keyboard by pressing return key or touching somewhere + added the transition of e-mail typed in one view to the another view

--- AvatarAppIos/Controllers/ConfirmationCodeViewController.swift
+++ AvatarAppIos/Controllers/ConfirmationCodeViewController.swift
@@ -22,10 +22,24 @@ class ConfirmationCodeViewController: UIViewController {
     @IBOutlet weak var wrongNumberPressed: UILabel!
     //--
     
-    let emailFromPreviousView = "prostopochta@gmail.com"
+    var emailFromPreviousView = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         nextStepButton.layer.cornerRadius = 8
         enteredEmail.text = emailFromPreviousView
+        self.enteredCodeLabel.delegate = self
+    }
+    
+    //Hide the keyboard by touching somewhere
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
+//MARK:- Hide the keyboard by pressing the return key
+extension ConfirmationCodeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
 }
--- AvatarAppIos/Controllers/EnteringEmailViewController.swift
+++ AvatarAppIos/Controllers/EnteringEmailViewController.swift
@@ -13,20 +13,48 @@ class EnteringEmailViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet private weak var nextStepButton: UIButton!
     @IBAction private func nextStepButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "Show Confirmation VC", sender: sender)
+        if emailField.text != "" {
+            performSegue(withIdentifier: "Show Confirmation VC", sender: sender)
+        }
+        else {
+            showEmailWarningAlert()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nextStepButton.layer.cornerRadius = 8
+        self.emailField.delegate = self
     }
-    //hello 
-    /*
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Show Confirmation VC" {
             let destinationVC = segue.destination as! ConfirmationCodeViewController
-            let button = sender as! UIButton
+            destinationVC.emailFromPreviousView = emailField.text!
         }
     }
-    */
+    
+//Hide the keyboard by touching somewhere
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+}
+
+//MARK:- Show warning alert about incorrect e-mail
+extension EnteringEmailViewController {
+    func showEmailWarningAlert(){
+        let alert = UIAlertController(title: "Некорректный адрес почты", message: "Пожалуйста, введите почту заново", preferredStyle: .alert)
+        let okBtn = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okBtn)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+//MARK:- Hide the keyboard by pressing the return key
+extension EnteringEmailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
 }
